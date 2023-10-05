8707df0010d6c8f416c5c63bb93ab82755097acb
pcho0712 <pcho0712@gmail.com>
2023-10-06 07:46:35 +0900

    home to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home: React.FC = () => {
+const Index: React.FC = () => {
   return (
     <div>
       <h1>Welcome to CityCam</h1>
@@ -9,4 +9,4 @@ const Home: React.FC = () => {
   );
 }
 
-export default Home;
+export default Index;
